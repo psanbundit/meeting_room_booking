c8cb7162432fb75cf0ab25eb743a7f4c975bf39c
Purinat Sanbundit <purinatsanbundit@Purinats-MacBook-Pro.local>
2023-09-26 16:37:59 +0700

    feat: update my booking page

--- mobile/lib/main/main_injector.dart
+++ mobile/lib/main/main_injector.dart
@@ -1,5 +1,6 @@
 import 'package:meeting_room_booking/common/bloc/application_cubit.dart';
 import 'package:meeting_room_booking/pages/booking_summary_page/bloc/booking_summary_cubit.dart';
+import 'package:meeting_room_booking/pages/my_booking_page/bloc/my_booking_cubit.dart';
 import 'package:meeting_room_booking/pages/search_room/bloc/search_room_cubit.dart';
 import 'package:root_dependencies/root_dependencies.dart';
 
@@ -9,6 +10,7 @@ class MainInjector extends BlocInjector {
     inject(ApplicationCubit());
     inject(SearchRoomPageCubit(), lazy: true);
     inject(BookingSummaryCubit(), lazy: true);
+    inject(MyBookingCubit(), lazy: true);
   }
 }
 
